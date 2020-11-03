a0e405d07fa7a76a5bc6a3bd8df3891990569064
sehamsamy <sehamsamy755@gmail.com>
2020-11-03 14:11:33 +0200

    all

--- resources/views/admin/clients_subscriptions/meals.blade.php
+++ resources/views/admin/clients_subscriptions/meals.blade.php
@@ -5,13 +5,14 @@
     <td>{{ $type->name }}</td>
     @for($i=1;$i<=7;$i++)
         <td>
-        
+
             <div class="{{$i}}" style="display: inline">
             @foreach($type->meals_sub($id) as  $key=>$meal)
+            <li>
               {{$meal->ar_name}}-{{$meal->calories}}
 
                      <input type="radio" id="{{$meal->id}}_{{$i}}" class="{{$i}}" name="meals[{{$meal->id}}][{{ $i }}]">
-
+            </li>
             @endforeach
         </div>
         </td>
