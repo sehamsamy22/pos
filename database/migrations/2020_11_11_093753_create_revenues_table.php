e7b4adf22f2bc28f952592372d97857c98e9b3d1
sehamsamy <sehamsamy755@gmail.com>
2020-11-15 19:45:24 +0200

    all_updates

--- database/migrations/2020_11_11_093753_create_revenues_table.php
+++ database/migrations/2020_11_11_093753_create_revenues_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaymentsTable extends Migration
+class CreateRevenuesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -32,6 +32,6 @@ class CreatePaymentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('payments');
+        Schema::dropIfExists('revenues');
     }
 }
