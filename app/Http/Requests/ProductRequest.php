43ce91f139da4eb6cca75885745113b68b64dd55
seham <sehamsamy755@gmail.com>
2020-12-04 21:17:01 +0200

    barcode

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -33,7 +33,7 @@ class ProductRequest extends FormRequest
            "unit" => "required|string|min:1|max:255",
            'image' => 'sometimes|file|image|mimes:jpeg,png,jpg,gif,svg|max:5000',
            "price" => "required",
-           "barcode" => "required|unique:products,barcode".$this->request['barcode']
+           "barcode" => "required|unique:products,barcode"
         ];
 
     }
