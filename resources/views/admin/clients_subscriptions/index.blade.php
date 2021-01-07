d9a87ed36ffeab856d2011ce7a219db4a851e78c
seham <sehamsamy755@gmail.com>
2021-01-07 18:37:08 +0200

    crorn

--- resources/views/admin/clients_subscriptions/index.blade.php
+++ resources/views/admin/clients_subscriptions/index.blade.php
@@ -74,33 +74,33 @@
                                 {!!Form::close() !!}
                             </td>
                         </tr>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal-{{$row->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">أيقاف إشتراك مؤقت  </h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    {!!Form::model($row, ['route' => ['dashboard.subscriptions.dis_active' ,$row->id] , 'method' => 'POST' ,'files'=>true]) !!}
+                                    <div class="modal-body">
 
+                                        <input type="date" class="form-control" name="end" id="" value={{$row->end}}>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-primary"> حفظ التعديلات</button>
+                                    </div>
+                                    {!!Form::close() !!}
+                                </div>
+                            </div>
+                        </div>
                     @endforeach
                     </tbody>
                 </table>
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal-{{$row->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">أيقاف إشتراك مؤقت  </h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            {!!Form::model($row, ['route' => ['dashboard.subscriptions.dis_active' ,$row->id] , 'method' => 'POST' ,'files'=>true]) !!}
-                            <div class="modal-body">
 
-                                <input type="date" class="form-control" name="end" id="" value={{$row->end}}>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary"> حفظ التعديلات</button>
-                            </div>
-                            {!!Form::close() !!}
-                        </div>
-                    </div>
-                </div>
     </div>
     </div>
 @endsection
