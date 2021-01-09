073a5ab33b3a4d95f8f547e38b730257519048c1
seham <sehamsamy755@gmail.com>
2021-01-09 02:58:43 +0200

    roles

--- database/migrations/2021_01_08_233144_add_role_id_to_users_table.php
+++ database/migrations/2021_01_08_233144_add_role_id_to_users_table.php
@@ -14,7 +14,7 @@ class AddRoleIdToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->nullable()->constrained('roles')->cascadeOnDelete();
+            $table->unsignedBigInteger('role_id')->nullable()->constrained('roles')->cascadeOnDelete();
 
         });
     }
