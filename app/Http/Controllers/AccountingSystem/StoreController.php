4efa33c0451d384557433ac4dc28bb5646b1a763
seham <sehamsamy755@gmail.com>
2021-01-11 04:31:15 +0200

    all

--- app/Http/Controllers/AccountingSystem/StoreController.php
+++ app/Http/Controllers/AccountingSystem/StoreController.php
@@ -71,7 +71,7 @@ else{
             $meals_=SubscriptionMeal::whereIn('subscription_id',$subcriptipns_id)->pluck('meal_id','id')->toArray();
             $allmeals=Meal::whereIn('id',$meals_)->pluck('id')->toArray();
             $products=MealProduct::whereIn('meal_id',$allmeals)->pluck('product_id','id')->toArray();
-         dd($products);
+//         dd($products);
             $storeproducts=StoreProduct::whereIn('product_id',$products)->get();
 //        dd($storeproducts);
          }else
