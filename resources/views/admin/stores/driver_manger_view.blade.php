8d96ef92518562f5155fc863849e0b4c6749d5b6
seham <sehamsamy755@gmail.com>
2021-01-12 19:14:56 +0200

    all

--- resources/views/admin/stores/driver_manger_view.blade.php
+++ resources/views/admin/stores/driver_manger_view.blade.php
@@ -79,7 +79,7 @@
 
                                                                  <select name="user_id" id="user_id-{{$row->id}}" class="form-control" >
                                                                 <?php
-                                                                     $drivers=\App\User::where('role','1')->where('area_id',$row->area_id)->get();
+                                                                     $drivers=\App\User::where('role','1')->get();
                                                                      ?>
                                                                    @foreach($drivers as $user)
                                                                     <option value="{{$user->id}}">{{$user->name}}</option>
